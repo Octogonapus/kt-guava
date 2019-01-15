f900b7f3b9b186a7f9acda487c74783f4176382d
Ryan Benasutti <firey45@gmail.com>
2019-01-15 11:52:50 -0500

    Add mapNodes
    Closes #1

---
+++ kt-guava/src/main/kotlin/org/octogonapus/guavautil/collections/ImmutableValueGraphUtil.kt
@@ -0,0 +1,63 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.octogonapus.guavautil.collections
+
+import com.google.common.graph.ImmutableValueGraph
+import com.google.common.graph.MutableValueGraph
+import com.google.common.graph.ValueGraph
+import com.google.common.graph.ValueGraphBuilder
+
+@Suppress("UnstableApiUsage")
+fun <N : Any, V : Any> ValueGraph<N, V>.toImmutableValueGraph(): ImmutableValueGraph<N, V> =
+    ImmutableValueGraph.copyOf(this)
+
+/**
+ * Maps the nodes of the [ValueGraph].
+ *
+ * @param transform A mapping function applied to the nodes.
+ * @return A graph with mapped nodes.
+ */
+@Suppress("UnstableApiUsage")
+fun <N : Any, V : Any, R : Any> ValueGraph<N, V>.mapNodes(
+    transform: (N) -> R
+): ValueGraph<R, V> = mapValueGraphNodes(transform)
+
+/**
+ * Maps the nodes of the [ValueGraph].
+ *
+ * @param transform A mapping function applied to the nodes.
+ * @return A graph with mapped nodes.
+ */
+@Suppress("UnstableApiUsage")
+fun <N : Any, V : Any, R : Any> ImmutableValueGraph<N, V>.mapNodes(
+    transform: (N) -> R
+): ImmutableValueGraph<R, V> = mapValueGraphNodes(transform).toImmutableValueGraph()
+
+@Suppress("UnstableApiUsage")
+private fun <N : Any, R : Any, V : Any> ValueGraph<N, V>.mapValueGraphNodes(
+    transform: (N) -> R
+): MutableValueGraph<R, V> {
+    val builder = if (isDirected) ValueGraphBuilder.directed() else ValueGraphBuilder.undirected()
+    val mutableGraph = builder.expectedNodeCount(nodes().size).build<R, V>()
+    val nodesRemapped = nodes().map { it to transform(it) }.toMap()
+
+    nodesRemapped.values.forEach { mutableGraph.addNode(it) }
+
+    edges().forEach {
+        // We just put these elements in the `nodesRemapped` above. They MUST be in the map.
+        val newNodeU = nodesRemapped[it.nodeU()]!!
+        val newNodeV = nodesRemapped[it.nodeV()]!!
+        mutableGraph.putEdgeValue(
+            newNodeU,
+            newNodeV,
+            edgeValue(it.nodeU(), it.nodeV()).orElseThrow {
+                IllegalStateException("Value was not present.")
+            }
+        )
+    }
+
+    return mutableGraph
+}
---
+++ kt-guava/src/test/kotlin/org/octogonapus/guavautil/collections/ImmutableValueGraphUtilTest.kt
@@ -0,0 +1,50 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.octogonapus.guavautil.collections
+
+import com.google.common.graph.MutableValueGraph
+import com.google.common.graph.ValueGraphBuilder
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import kotlin.test.assertEquals
+
+internal class ImmutableValueGraphUtilTest {
+
+    @Test
+    fun `test toImmutableValueGraph`() {
+        val graph = makeMockValueGraph()
+        val immutableGraph = graph.toImmutableValueGraph()
+
+        assertAll(
+            { assertEquals(graph.nodes(), immutableGraph.nodes()) },
+            { assertEquals(graph.edges(), immutableGraph.edges()) }
+        )
+    }
+
+    @Test
+    fun `test mapNodes with mutable receiver`() {
+        val graph = makeMockValueGraph()
+        val graphMapped = graph.mapNodes { it + 1 }
+
+        assertEquals(graphMapped.nodes(), setOf(1, 2))
+    }
+
+    @Test
+    fun `test mapNodes with immutable receiver`() {
+        val graph = makeMockValueGraph()
+        val graphMapped = graph.toImmutableValueGraph().mapNodes { it + 1 }
+
+        assertEquals(graphMapped.nodes(), setOf(1, 2))
+    }
+
+    @Suppress("UnstableApiUsage")
+    private fun makeMockValueGraph(): MutableValueGraph<Int, Int> =
+        ValueGraphBuilder.undirected().build<Int, Int>().apply {
+            addNode(0)
+            addNode(1)
+            putEdgeValue(0, 1, 1)
+        }
+}
